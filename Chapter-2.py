0b0c386f2aa6577a32bbc110e76008255ca6f869
TristaWWP <31362022+TristaWWP@users.noreply.github.com>
2019-11-21 13:44:38 +0800

    第二章习题答案

---
+++ Chapter-2.py
@@ -0,0 +1,65 @@
+"""
+2-1
+"""
+message = "一条消息"
+print(message)
+
+
+"""
+2-2
+"""
+message = "一条消息"
+print(message)
+
+message = "新消息"
+print(message)
+
+"""
+2-3
+"""
+name = "wwp"
+print("Hello " + name + ", would u like to learn some python today")
+
+"""
+2-4
+"""
+name = "wwp"
+print(name.lower())#小写
+print(name.upper())#大写
+print(name.title())#首字母大写
+
+"""
+2-5
+"""
+name = "wwp"
+sentence = '"热爱生活，无问西东"'#引号的嵌套
+print(name + " once said, " + sentence)
+
+"""
+2-6
+"""
+famous_person = "wwp"
+message = '"热爱生活，无问西东"'
+print(famous_person + " once said, " + message)
+
+"""
+2-7
+"""
+name = "  \twwp  "
+print(name.rstrip())#去除右边空白
+print(name.lstrip())#去除左边空白
+print(name.strip())#去除两边空白
+
+"""
+2-8
+"""
+print(5 + 3)
+print(9 - 1)
+print(2 * 4)
+print(8 / 1)
+
+"""
+2-9
+"""
+num = 13
+print("My favorite num is " + str(num))#str数字转字符串
